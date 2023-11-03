c0ff6a5f2127b93b776f0ab8db315b158fac8e7a
Priyanka Vaidya <vaidyapriyanka80@gmail.com>
2023-11-04 00:19:53 +0530

    pdf pages displaying error fixed

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -11,8 +11,11 @@ const FileUpload = () => {
     useEffect(() => {
         // Retrieve the PDF data from localStorage
         const storedPDFData = localStorage.getItem('pdfData');
-        setPdfData(storedPDFData);
-    }, []);
+        if(storedPDFData){
+            setPdfData(storedPDFData);
+        }
+        // console.log(storedPDFData);
+    }, [pdfData]);
 
     const onDocumentLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
@@ -35,12 +38,14 @@ const FileUpload = () => {
         reader.onload = (event) => {
             const pdfData = event.target.result;
             localStorage.setItem('pdfData', pdfData);
+            setPdfData(pdfData);
         }
         reader.readAsDataURL(file);
     }
 
     const onFileInputChange = (event) => {
         const file = event.target.files[0];
+        // console.log(file);
         if(file) {
             handleFileUpload(file);
         }
@@ -48,7 +53,7 @@ const FileUpload = () => {
 
 
     return (
-        <div>
+        <>
             <div className='mt-[40px] mb-10 py-2 px-4 w-fit font-bold rounded-[4px] bg-[#00cc99]'>
                     <div className='btn btn-medium text-white text-lg cursor-pointer' onClick={handleInputClick}>Edit a PDF document 
                         <span className='font-normal'> - it's free</span>
@@ -56,23 +61,36 @@ const FileUpload = () => {
                     <input className='hidden' type="file" accept='.pdf' title="Upload" name="file" ref={fileInputRef} onChange={ onFileInputChange } />
             </div>
             {/* ............ Display pdf here ......... */}
-            <div>
-                {pdfData && (
+            <div className="flex flex-wrap">
+                {pdfData ?  (
+
+                   
                     <Document 
                         file={pdfData}
                         onLoadSuccess={onDocumentLoadSuccess}
+                        className="grid xl:grid-cols-12 min-[991px]:grid-cols-12 grid-cols-12 mt-3 gap-4"
                     >
                         {Array.from(new Array(numPages),(el, index) => (
-                            <div key={`serial_${index + 1}`}>
-                            <input type="checkbox" />
-                            <Page key={`page_${index + 1}`} pageNumber={index + 1}  renderAnnotationLayer={false} renderTextLayer={false} />
+                            <div 
+                                key={`serial_${index + 1}`}
+                                className="p-3 max-[768px]:col-span-12 min-[768px]:col-span-4 min-[1300px]:col-span-3 mx-3">
+                                <input type="checkbox" />{index + 1}
+                                <Page 
+                                    key={`page_${index + 1}`}
+                                    pageNumber={index + 1}
+                                    renderAnnotationLayer={false}
+                                    renderTextLayer={false}
+                                    width={200} />
                             </div>
                           )
                          )}
                     </Document>
-                )}
+                ) : (
+                    <div>No PDF selected</div>
+                )
+                }
             </div>
-        </div>
+        </>
     )
 }
 
